ae69e44d16c6211723a47662ceb76d51b33d4256
mkain0 <kain.magali@gmail.com>
2016-01-25 13:03:38 -0300

    Modified getByCategory.

--- shoppingCart/src/main/java/com/shoppingCart/app/controller/ProductController.java
+++ shoppingCart/src/main/java/com/shoppingCart/app/controller/ProductController.java
@@ -40,10 +40,10 @@ public class ProductController {
 		return new ResponseEntity<Product> (product, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/products?category={category}", method = RequestMethod.GET)
-	public @ResponseBody ResponseEntity<List<Product>> getByCategory(@PathVariable("category") String category) throws ProductNotFoundException  {
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+	public @ResponseBody ResponseEntity<List<Product>> getByCategory(@RequestParam("category") String category) throws ProductNotFoundException  {
 		List<Product> products = productService.findByCategory(category);
 		return new ResponseEntity<List<Product>> (products, HttpStatus.OK);
 	}
-	
+
 }
